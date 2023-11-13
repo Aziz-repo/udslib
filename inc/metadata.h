343baec7c32f9cddaad07b5e7c22b3f12499423b
PC-AES-L5235-LIN <mohamed-aziz.hassen@actia.engineering.tn>
2023-11-13 10:05:26 +0100

    added metadata for the services ids

--- inc/metadata.h
+++ inc/metadata.h
@@ -2,6 +2,8 @@
 #define SERVICE_ID_SIZE              2
 #define SERVICE_SUBFUNCTION_SIZE     1
 
-#define RDBI                      0x22
-#define WDBI                      0x2E
-#define ECUR                      0x11
+#define RDBI                      0x22 // Read Data By Identifer
+#define WDBI                      0x2E // Write Data By Identifer
+#define ECUR                      0x11 // ECU Reset 
+#define CDI                       0x14 // Clear DTC Information
+#define CDS                       0x85 // Control DTC Settings
