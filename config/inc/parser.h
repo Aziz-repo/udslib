e1ee7f187d5c1b214f5393f81b5e756cbf57bd5c
Aziz-repo <azizhassen69@gmail.com>
2023-11-21 10:01:01 +0100

    added the configuration parser module

---
+++ config/inc/parser.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <algorithm>
+#include <cctype>
+#include <filesystem>
+#include <iterator>
+#include <map>
+#include <string>
+
+namespace uds {
+class UDSConfiguration {
+public:
+  UDSConfiguration() = default;
+  ~UDSConfiguration() = default;
+
+  void loadConfiguration(std::filesystem::path &);
+  std::string getConfiguration(std::string &);
+  void trimConfiguration(std::string &);
+
+private:
+
+  static inline std::string &ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+                                    [](int c) { return !std::isspace(c); }));
+    return s;
+  }
+
+  static inline std::string &rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+                         [](int c) { return !std::isspace(c); })
+                .base(),
+            s.end());
+    return s;
+  }
+
+  std::map<std::string, std::string> configuration_options_;
+};
+} // namespace uds
---
+++ config/src/parser.cpp
@@ -0,0 +1,31 @@
+#include "parser.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace uds;
+
+void UDSConfiguration::loadConfiguration(std::filesystem::path& config_path) {
+  std::ifstream config_file(config_path);
+  std::string content;
+  if (config_file.is_open()) {
+    while(!config_file.eof()) {
+      std::string line;
+      std::getline(config_file, line);
+      content += line + "\n";
+    }
+    std::cout << content << std::endl;
+  } else {
+    std::cerr << "Cannot open file\n";
+    return;
+  }
+}
+
+// Dummy implementation
+std::string UDSConfiguration::getConfiguration(std::string& s) {
+  return s;
+}
+
+void UDSConfiguration::trimConfiguration(std::string& s) {
+  UDSConfiguration::ltrim(UDSConfiguration::rtrim(s));
+}
