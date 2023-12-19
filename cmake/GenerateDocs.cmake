11e38641bee7b693a2d8ea962230ce8b6ee2c593
Aziz-repo <azizhassen69@gmail.com>
2023-12-19 11:58:07 +0100

    fixed doxygen intergration with cmake

--- cmake/GenerateDocs.cmake
+++ cmake/GenerateDocs.cmake
@@ -1,18 +1,45 @@
 # Adapted from https://vicrucann.github.io/tutorials/quick-cmake-doxygen/
 
+# TODO: fix this bs
 find_package(Doxygen)
 if (DOXYGEN_FOUND)
   set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile.in)
-  set(DOXYGEN_OUT ${CMAKE_BINARY_DIR}/Doxyfile)
+  set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
 
   configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
   message(STATUS "Doxygen build started")
 
-  add_custom_target(doc ALL
-    COMMAND ${DOXYGEN_EXCUTABLE} ${DOXYGEN_IN}
+add_custom_command(
+    OUTPUT remove_doc
+    COMMAND ${CMAKE_COMMAND} -E remove_directory doc/
+    VERBATIM
+  )
+
+  add_custom_command(
+    OUTPUT clean_doc
+    COMMAND rm -rf doc/* Doxyfile CMakeDoxyfile.in CMakeDoxygenDefaults.cmake
+    VERBATIM
+  )
+
+  add_custom_command(
+    OUTPUT doc_dir
+    COMMAND ${CMAKE_COMMAND} -E make_directory doc
+  )
+
+  add_custom_target(doc
+    DEPENDS doc_dir
+    COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+    MAIN_DEPENDENCY ${DOXYGEN_OUT} ${DOXYGEN_IN}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     COMMENT "Generating API documentation with Doxygen"
     VERBATIM 
+  )
+
+  add_custom_target(doc_clean
+    DEPENDS remove_doc clean_doc
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    COMMENT "Cleaning docs"
+    VERBATIM
   )
 else (DOXYGEN_FOUND)
   message(ERROR "Doxgen need to be installed to generate doxygen documentation")
